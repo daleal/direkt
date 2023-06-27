07d5f9d64bb91c97b625f44ef596da1c81d59037
Daniel Leal <cosaleal@gmail.com>
2023-06-26 21:45:24 -0400

    chore: add a base functioning application

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({ registerType: 'autoUpdate' }),
+  ],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+});
