7b4bbb2d612c1032d8e7549078e22fd14ba8022b
Daniel Leal <cosaleal@gmail.com>
2023-06-26 21:14:18 -0400

    chore: remove old code

--- src/services/maps.js
+++
@@ -1,1 +0,0 @@
-export const geo = (direction) => `https://www.google.com/maps/search/?api=1&query=${encodeURI(direction)}`;
--- vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  transpileDependencies: [
-    'vuetify',
-  ],
-  pwa: {
-    themeColor: '#D81B60',
-  },
-};
