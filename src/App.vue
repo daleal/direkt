6db10335246552765165fee257e811705aca0975
Daniel Leal <cosaleal@gmail.com>
2023-07-20 00:44:33 -0400

    fix: weird view height bug

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,34 @@
 <script setup lang="ts">
+import { onMounted, onUnmounted } from 'vue';
 import { GApplication } from 'geometr/components';
+
+/**
+ * A hack to be able to use the view height attribute
+ * when on mobile and avoid weird overflow bugs.
+ */
+const setViewHeight = () => {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+};
+
+onMounted(() => {
+  setViewHeight();
+  window.addEventListener('resize', setViewHeight);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', setViewHeight);
+});
 </script>
 
 <template>
-  <GApplication>
+  <GApplication class="application">
     <router-view />
   </GApplication>
 </template>
+
+<style scoped>
+div .application {
+  height: calc(var(--vh) * 100);
+}
+</style>
